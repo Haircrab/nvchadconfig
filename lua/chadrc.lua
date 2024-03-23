7244dcc9bef812df072af291e1617bc77348926e
Ken Wong <kiuare310@gmail.com>
2024-03-23 13:18:13 +0800

    theme

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,40 +1,14 @@
 ---@type ChadrcConfig
-
-local palenight = require "themes.palenight"
 local M = {}
 
 M.ui = {
-  changed_themes = {
-    palenight = {
-      base_16 = palenight.base_16,
-      base_30 = palenight.base_30,
-    },
-  },
-  theme = "palenight",
+	theme = "palenight-custom",
 }
 
-if M.ui.theme == "palenight" then
-  M.ui.hl_override = {
-    ["@variable"] = { fg = palenight.base_30.lavender },
-
-    ["@keyword"] = { fg = palenight.base_30.cyan },
-    Repeat = { fg = palenight.base_30.cyan },
-    Operator = { fg = palenight.base_30.cyan },
-    ["@operator"] = { fg = palenight.base_30.cyan },
-
-    ["@property"] = { fg = palenight.base_16.base04 },
-    ["@type.builtin"] = { fg = palenight.base_30.purple },
-    Typedef = { fg = palenight.base_30.yellow },
-    Type = { fg = palenight.base_30.yellow },
-    Structure = { fg = palenight.base_30.yellow },
-
-    ["@punctuation.bracket"] = { fg = palenight.base_30.cyan },
-    -- ["@parenthesis"] = { fg = palenight.base_30.dark_purple },
-
-    Comment = {
-      italic = true,
-    },
-  }
+local success, themeModule = pcall(require, 'themes.' .. M.ui.theme)
+if success then
+  M.ui.hl_override = themeModule.polish_hl
 end
+
 
 return M
